f4c2636b433054a878235297e73ef2c493d91c27
ZubairTTN <mohd.ahmed@tothenew.com>
2025-03-24 14:56:20 +0530

    WrapClass

---
+++ IntellijPrac/com/tothenew/enumDeo/Demo1.java
@@ -0,0 +1,9 @@
+package com.tothenew.enumDeo;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println(DayClass.MONDAY);
+        System.out.println(DayClass.TUESDAY); //here we are using enums.
+        System.out.println(DayClass.WEDNESDAY);
+    }
+}
